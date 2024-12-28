9758d235b42cce7c6b13fdca4721bb06470e160f
obedeDintala123 <obededintala@gmail.com>
2024-12-28 20:37:58 +0100

    Adicionando estilo na tela de Login

--- src/compontes/screens/formComponents/Input.js
+++ src/compontes/screens/formComponents/Input.js
@@ -1,6 +1,6 @@
 import '../../../assents/style/output.css';
 const Input = ({type, placeholder, name, id}) => {
-    return <input className='w-full outline-none bg-[#F2F4F7] p-3 rounded' type={type} placeholder={placeholder} name={name} id={id} />
+    return <input className='w-full outline-none bg-[#F2F4F7] p-2 rounded' type={type} placeholder={placeholder} name={name} id={id} />
 }
 
 export default Input;
--- src/compontes/screens/signIn.js
+++ src/compontes/screens/signIn.js
@@ -5,7 +5,7 @@ import Button from './formComponents/Button';
 const SignIn = () => {
     return (
      
-            <form className='lg:w-4/12 flex flex-col gap-6 justify-between p-6 border shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-sm:w-11/12 sm:w-11/12 md:w-3/5'>
+            <form className='lg:w-3/12 flex flex-col gap-6 justify-between p-3 border shadow absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 max-sm:w-11/12 sm:w-11/12 md:w-3/5'>
             <div className='flex flex-col gap-6'>
                 <img src={logo} alt='logo' className='w-20 text-center'/>
                 <h1 className='text-2xl font-medium'>Sign In</h1>
@@ -16,7 +16,7 @@ const SignIn = () => {
            <Input type="password" placeholder="Senha" name="senha" id="senha" />
             <Button type="submit" text="Sign In" name="signInButton" id="signInButton" />
           </div>
-            <p className='text-xs'>Don't have an account? <a href="/signup">Sign Up</a></p>
+            <p className='text-sm'>Don't have an account? <a href="/signup" className='underline font-medium'>Sign Up</a></p>
         </form>
       
     )
